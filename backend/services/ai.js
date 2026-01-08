998e0822aab44242cc57f658ea1fe014d449a72d
aweston <austin.inhyuk.chong@gmail.com>
2026-01-08 10:32:48 -0800

    Prompt Engineering

--- backend/services/ai.js
+++ backend/services/ai.js
@@ -1,5 +1,9 @@
 import "dotenv/config";
-import { GoogleGenAI, createUserContent, createPartFromUri } from "@google/genai";
+import {
+  GoogleGenAI,
+  createUserContent,
+  createPartFromUri,
+} from "@google/genai";
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({});
@@ -14,18 +18,29 @@ async function prompt(job, resumePath) {
     model: "gemini-2.5-flash",
     contents: createUserContent([
       createPartFromUri(myfile.uri, myfile.mimeType),
-      `Given this job description, provide me the company name, company description, 
-    company industry, company size, company stage, job name, job description, job qualifications, 
-    job experience level, job salary, team of the role, and location.
-    
-    Now, given the attached resume, include fields of "how to be of impact" and "what the company currently needs", give these
-    two fields a label of "impact" and "company_need". Now include 3 potential technical questions for the interview and 3
-    questions to ask the interviewer, include answers based off my resume and job description. Give me 3 points to higlight
-    off of my resume that align with the job description. Finally, include a link from Reddit, Glassdoor, and Blind about interview experiences from this company.
+      `Given this job description, ${job}, and my resume, please output a JSON object of the following fields:
 
-    Please output a JSON object, each key-value pair corresponding to the desired fields 
-    I specified: ${job}
-    `,
+       company name, 
+       company description, 
+       company industry, 
+       company size, 
+       company stage, 
+
+       job name, 
+       job description, 
+       job qualifications, 
+       job experience level, 
+       job salary, 
+       job team,
+       job location,
+
+       how to be of impact as "impact", 
+       what the company currently needs as "company_need", 
+       3 potential technical interview questions as "technical_interview_questions",
+       3 questions to ask the interviewer as "interviewer_questions",
+       3 stories to highlight off my resume as "higlight",
+       analysis of how my skills align with the role and company as "analysis"
+      `,
     ]),
   });
 
